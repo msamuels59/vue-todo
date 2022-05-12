08aa6fb136414f47320fbb82ea0b341510cdc3d2
Mike Samuels <msamuels59@gmail.com>
2022-05-12 16:43:00 -0400

    new store appraoch

--- src/store/index.ts
+++ src/store/index.ts
@@ -24,23 +24,23 @@ import {
  })
 
  export function useStore() {
-   return store as Store
+   return store 
  }
 
- export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch' | 'modules'> & {
-   commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
-     key: K,
-     payload: P,
-     options?: CommitOptions
-   ): ReturnType<Mutations[K]>
- } & {
-   dispatch<K extends keyof Actions>(
-     key: K,
-     payload?: Parameters<Actions[K]>[1],
-     options?: DispatchOptions
-   ): ReturnType<Actions[K]>
- } & {
-   getters: {
-     [K in keyof Getters]: ReturnType<Getters[K]>
-   }
- }
+//  export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch' | 'modules'> & {
+//    commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+//      key: K,
+//      payload: P,
+//      options?: CommitOptions
+//    ): ReturnType<Mutations[K]>
+//  } & {
+//    dispatch<K extends keyof Actions>(
+//      key: K,
+//      payload?: Parameters<Actions[K]>[1],
+//      options?: DispatchOptions
+//    ): ReturnType<Actions[K]>
+//  } & {
+//    getters: {
+//      [K in keyof Getters]: ReturnType<Getters[K]>
+//    }
+//  }
