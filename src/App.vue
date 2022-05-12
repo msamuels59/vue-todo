1c8811ca10c563cbf7499308f0afd93d31a4b3ef
Mike Samuels <msamuels59@gmail.com>
2022-05-11 21:58:46 -0400

    added data to main page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container mx-auto mt-4">
+    <h1 class="is-size-3 has-text-centered p-2 has-text-weight-bold">
+      Today's To Dos
+    </h1>
+    <div v-if="loading">
+      <h3 class="has-text-centered mt-4">Loading...</h3>
+    </div>
+    <div v-else>
+      <p class="has-text-centered mt-2">
+        {{ completedCount }} of {{ totalCount }} completed
+      </p>
+      <TaskList />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, defineComponent, onMounted } from 'vue';
+import TaskList from './components/TaskList.vue';
+import { userStore } from './store';
+import { ActionTypes } from './store/actions';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'ToDo',
+  components: { TaskList },
+  setup() {
+    const store = userStore()
+    const loading = computed(() => store.state.loading)
+    onMounted(() => store.dispatch(ActionTypes.GetTaskItems))
+    const completedCount = computed(() => store.getters.completedTaskCount)
+    const totalCount = computed(() => store.getters.totalTaskCount)
+    return { loading, completedCount, totalCount}
   }
 });
 </script>
